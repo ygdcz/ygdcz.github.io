b89f7eb5fc9baf2a4c2caca1ab72e98e1f56d187
ygdcz <50568690+ygdcz@users.noreply.github.com>
2021-01-21 15:45:40 +0800

    Update index.js

--- image_animation/js/index.js
+++ image_animation/js/index.js
@@ -1,13 +1,3 @@
-// const $banner = $('.banner'),
-//   $blocks = $('.blocks'); 
-// let blocks = ""; 
-// console.log($blocks);
-// for (var index = 1; index < 400; index++) {
-//   blocks += "<div class='blocks'></div>";
-//   $blocks.css("animationDelay", `{index * 0.05}s`) ;
-// }
-
-// $banner.html(blocks);
 
 const banner = document.getElementsByClassName('banner')[0];
 const blocks = document.getElementsByClassName('blocks');
